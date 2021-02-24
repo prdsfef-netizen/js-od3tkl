3ab4aa3fef1fb290cec59b9e54e2b997f4a3e954
prdsfef-netizen <cepd98@gmail.com>
2021-02-24 16:34:50 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-od3tkl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-od3tkl)
---
+++ index.js
@@ -0,0 +1,23 @@
+// Import stylesheets
+import "./style.css";
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  console.log(position);
+  x.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+}
+
+getLocation();
